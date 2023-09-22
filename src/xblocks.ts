0037c9764386ee32235b6ca9470915d635c981ae
Steven Obua <steven@recursivemind.com>
2023-09-22 17:53:22 +0100

    xblocks

--- src/xblocks.ts
+++ src/xblocks.ts
@@ -6,12 +6,13 @@ export interface Ephemerals {
     [key : string] : unknown;
 }
 
-export type Block = { parts : Part[] }  
+export type Document = { ephemerals : Ephemerals, blocks : Block[] }
+export type Block = { ephemerals : Ephemerals, parts : Part[] }  
 export type Part = Line | Block
-export type Line = { fragments : Fragment[] }
+export type Line = { ephemerals : Ephemerals, fragments : Fragment[] }
 export type Fragment = Text | Span
-export type Span = { fragments : (Text | Span)[] }
-export type Text = { text : string }
+export type Span = { ephemerals : Ephemerals, fragments : (Text | Span)[] }
+export type Text = { ephemerals : Ephemerals, text : string }
 
 export function isLine(part : Part) : part is Line {
     return Object.hasOwn(part, 'fragments');
@@ -36,7 +37,9 @@ function spaces(num : nat) : string {
     return s;
 }
 
-export function writeBlock(block : Block, indent : nat) : string[] {
+export function writeDocument(document : Document, indent : nat) : string[] {
+
+    const prefix = spaces(indent);    
 
     function wText(text : Text) : string {
         return text.text;
@@ -62,7 +65,6 @@ export function writeBlock(block : Block, indent : nat) : string[] {
     }
 
     function indentBlock(lines : string[]) {
-        const prefix = spaces(indent);
         for (let i = 0; i < lines.length; i++) {
             lines[i] = prefix + lines[i];
             if (i > 0) lines[i] = prefix + lines[i];
@@ -83,53 +85,120 @@ export function writeBlock(block : Block, indent : nat) : string[] {
         return lines;
     }
 
-    return wBlock(block);
+    function indentTopBlock(lines : string[]) {
+        for (let i = 1; i < lines.length; i++) {
+            if (i > 0) lines[i] = prefix + lines[i];
+        }
+    }
+
+    function wDocument(document : Document) : string[] {
+        let lines : string[] = [];
+        for (const block of document.blocks) {
+            const blockLines = wBlock(block);
+            indentTopBlock(blockLines);
+            lines.push(...blockLines);
+        }
+        return lines;    
+    }
+
+    return wDocument(document);
+}
+
+let nextKeyBase = 0;
+
+function newKey() : string {
+    return `key-` + (nextKeyBase++);
+}
+
+export function createEphemerals() : Ephemerals {
+    return { key : newKey() };
+}
+
+export function createDocument(...blocks : Block[]) : Document {
+    return { ephemerals : createEphemerals(), blocks : blocks }
+}
+
+export function createLine(...fragments : (Text | Span)[]) : Line {
+    return { ephemerals : createEphemerals(), fragments : fragments };
+}
+
+export function createBlock(...parts : Part[]) : Block {
+    return { ephemerals : createEphemerals(), parts : parts };
+}
+
+export function createSpan(...fragments : (Text | Span)[]) : Span {
+    return { ephemerals : createEphemerals(), fragments : fragments }
+}
+
+export function createText(text : string = "") : Text {
+    return { ephemerals : createEphemerals(), text : text };
 }
 
 function line(text? : string) : Line {
-    if (text === undefined) return { fragments: [] };
-    else return { fragments : [{text : text}] };
+    if (text === undefined) return createLine();
+    else return createLine(createText(text));
 }
 
 function block(...parts : (Line | Block)[]) : Block {
-    return { parts: parts };
+    return createBlock(...parts);
 }
 
-const exampleBlock : Block = block(
-    line("theory Pure"),
-    block(
+function paragraph(text : string) : Block {
+    return block(line(text));
+}
+
+const exampleDocument : Document = createDocument(
+    block( // comment block
+      line("% This is a comment."),
+      line("This is the second line of the comment."),
+      line("And here is the third one.")
+    ),
+    block( // theorem 
         line("theorem Modus-Ponens: B"),
-        block(line("premise: implies(A, B)")),
-        block(line("premise: A"))
+        paragraph("premise: implies(A, B)"),
+        paragraph("premise: A")
     ),
     block(
         line("theorem Universal-Introduction:"),
-        line("for-all(x. A[x])"),
-        block(line("premise: x. A[x]"))
+        paragraph("for-all(x. A[x])"),
+        paragraph("premise: x. A[x]")
     ),
     block(
         line("theorem Truth-1: true")
     ),
     block(
+        line("theorem Truth-2: implies(A, equals(A, true))")
+    ),
+    block(
+        line("theorem Implication-1:  implies(A, implies(B, A))")
+    ),
+    block(
         line("theorem Implication-2:"),
         block(
             line("implies"),
-            block(
-                block(line("implies(A, implies(B, C))")),
-                block(line("implies(implies(A, B), implies(B, C))"))
-            )
+            paragraph("implies(A, implies(B, C))"),
+            paragraph("implies(implies(A, B), implies(B, C))")
         )
-    )
+    ),
+    block(
+        line("theorem Universal-1: implies(for-all(x. A[x]), A[x])"),
+    ),
+    block(
+        line("theorem Universal-2:"),
+        block(
+            line("implies"),
+            paragraph("for-all(x. implies(A, B[x]))"),
+            paragraph("implies(A, for-all(x. B[x]))")
+        )
+    ),
+    paragraph("theorem Equality-1: equals(x, x)"),
+    paragraph("theorem Equality-2: implies(equals(x, y), implies(A[x], A[y]))")
 );
 
-const lines = writeBlock(exampleBlock, 2);
+const lines = writeDocument(exampleDocument, 2);
 for (const line of lines) {
-    console.log("| " + line);
+    console.log("|" + line);
 }
-
-
-
-
 
 
 /*
